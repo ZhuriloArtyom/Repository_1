d0570ed929e169189ce967cbbe9ed9c2e674c97c
ZhuriloArtyom <79194350+ZhuriloArtyom@users.noreply.github.com>
2021-02-23 01:25:11 +0300

    Created CreateArray

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
 
 
     }
+    private static int[] CreateArray(int length) {
+        return new int[length];
+    }
     private static int[] AddToArray(int index, int element, int[] Array) {
         Array[index] = element;
         return Array;
