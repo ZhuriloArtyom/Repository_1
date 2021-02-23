f9973613d4e61e76886ded19538f620a1fc39709
ZhuriloArtyom <79194350+ZhuriloArtyom@users.noreply.github.com>
2021-02-23 14:10:17 +0300

    Created class MainTest with CreateArrayTest and AddToArrayTest in it

--- src/Test/MainTest.java
+++ src/Test/MainTest.java
@@ -10,4 +10,11 @@ public class MainTest extends Assertions{
         int[] Array = Main.CreateArray(7);
         assertEquals(7, Array.length);
     }
+    @Test
+    public void AddToArrayTest(){
+        int[] Array = new int[3];
+        Array = Main.AddToArray(1,5, Array);
+        assertEquals(5, Array[1]);
+
+    }
 }
