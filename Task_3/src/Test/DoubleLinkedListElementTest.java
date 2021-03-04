bf6c710b471e48403e0a7380e78708d792f7ff94
ZhuriloArtyom <79194350+ZhuriloArtyom@users.noreply.github.com>
2021-03-04 22:43:10 +0300

    Added tests to DoubleLinkedListElementTest; Created DoubleLinkedListTest

--- Task_3/src/Test/DoubleLinkedListElementTest.java
+++ Task_3/src/Test/DoubleLinkedListElementTest.java
@@ -16,4 +16,23 @@ public class DoubleLinkedListElementTest extends Assertions{
         DoubleLinkedListElement_Interface Element2 = new DoubleLinkedListElement(Element1, null, null);
         assertEquals(Element1, Element2.GetPrevious() );
     }
+    @Test
+    public void ChangeNext_Test(){
+        DoubleLinkedListElement_Interface Element1 = new DoubleLinkedListElement(null, 98, null);
+        DoubleLinkedListElement_Interface Element2 = new DoubleLinkedListElement(null, null, null);
+        Element2.ChangeNext(Element1);
+        assertEquals(Element1,Element2.GetNext());
+    }
+    @Test
+    public void ChangePrevious_Test(){
+        DoubleLinkedListElement_Interface Element1 = new DoubleLinkedListElement(null, 98, null);
+        DoubleLinkedListElement_Interface Element2 = new DoubleLinkedListElement(null, null, null);
+        Element2.ChangePrevious(Element1);
+        assertEquals(Element1,Element2.GetPrevious());
+    }
+    @Test
+    public void GetData_Test(){
+        DoubleLinkedListElement_Interface Element1 = new DoubleLinkedListElement(null, 98, null);
+        assertEquals(98 ,Element1.GetData());
+    }
 }
