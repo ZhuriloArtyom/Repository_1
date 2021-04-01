ec5aa5d60960e443cf84ac9ae34525ed30035bb4
ZhuriloArtyom <79194350+ZhuriloArtyom@users.noreply.github.com>
2021-04-01 23:40:29 +0300

    Implemented Sort by search

--- Task_7/src/QuadraticSort.java
+++ Task_7/src/QuadraticSort.java
@@ -3,6 +3,19 @@ import java.util.Arrays;
 public class QuadraticSort {
     public void sort(int[] data)
     {
-        Arrays.sort(data); // Replace this
+        for (int i = data.length-1; i > 0; i--){
+            int ind_max;
+            int max;
+            ind_max = 0;
+            max = data[0];
+            for(int j =1; j<=i; j++) {
+                if (data[j] > max){
+                    max =  data[j];
+                    ind_max = j;
+                }
+            }
+            data[ind_max] = data[i];
+            data[i] = max;
+        }
     }
 }
